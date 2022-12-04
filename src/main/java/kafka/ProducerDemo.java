5266ae65b411dc7ebc660f0686ec94a8a3fbb816
alexander.fridman <alexander.fridman@over-c.com>
2022-12-04 13:09:10 +0000

    - simple producer

---
+++ src/main/java/kafka/ProducerDemo.java
@@ -0,0 +1,34 @@
+package kafka;
+
+import java.util.Properties;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+
+    public static void main(String[] args) {
+        log.info("I'm a Kafka Producer");
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+
+        producer.send(producerRecord); //async
+        producer.flush(); //sync
+        producer.close();
+    }
+}
